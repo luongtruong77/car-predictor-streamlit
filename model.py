85f9ccfa979f3bf5e6676604ea4fe4159928f194
Luong Q Truong <68078498+luongtruong77@users.noreply.github.com>
2021-04-15 12:11:05 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import joblib
-import xgboost as xgb
+
 
 st.write("""
 # Car's Price Prediction App
